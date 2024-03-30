86afef3688cd15af45e580912781eb110c6dd3ac
Nishant <nishantrathod484@gmail.com.com>
2024-03-31 00:11:47 +0530

    dashboard link

--- dev-mate/src/components/sidebar.tsx
+++ dev-mate/src/components/sidebar.tsx
@@ -7,10 +7,10 @@ function SideBar() {
   return (
     <div>
       <div className="w-64 h-screen bg-gray-100 dark:bg-gray-900 flex flex-col gap-5   px-14 py-4  border-r">
-        <h6 className=" flex gap-2 items-center  ">
+        <Link href="/dashboard"><h6 className=" flex gap-2 items-center  ">
           <LayoutDashboard />
           Dashboard
-        </h6>
+        </h6></Link>
      <Link href="/projects">
         <h6 className=" flex gap-2 items-center  ">
           <FileCode  />
@@ -22,20 +22,20 @@ function SideBar() {
           Hiring
         </h6></Link>
 
-        <h6 className=" flex gap-2 items-center  ">
+      <Link href="/leaderboard"> <h6 className=" flex gap-2 items-center  ">
         <BarChartBig />
           Leaderboard
-        </h6>
+        </h6></Link>
 
-        <h6 className=" flex gap-2 items-center  ">
+      <Link href="/pricing">  <h6 className=" flex gap-2 items-center  ">
         <IndianRupee />
           Pricing
-        </h6>
+        </h6></Link>
 
-        <h6 className=" flex gap-2 items-center  ">
+       <Link href="/accounts"> <h6 className=" flex gap-2 items-center  ">
         <CircleUserRound />
           Accounts
-        </h6>
+        </h6></Link>
       </div>
     </div>
   );
