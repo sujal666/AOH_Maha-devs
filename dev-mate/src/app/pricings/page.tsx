58b5debd21fd03534779aeb3bd4d5cc1bfaf79ee
rahulsan007 <santrarahul7070@gmail.com>
2024-03-31 01:14:55 +0530

    update

--- dev-mate/src/app/pricings/page.tsx
+++ dev-mate/src/app/pricings/page.tsx
@@ -1,18 +1,11 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
-// const page = () => {
-//   return (
-//     <div>page</div>
-//   )
-// }
-// export default page
-
-import { BellRing, Check} from "lucide-react"
-import Link from 'next/link';
+import { BellRing, Check } from "lucide-react";
+import Link from "next/link";
 // import { checkout } from "@/components/checkout";
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -20,8 +13,8 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Switch } from "@/components/ui/switch"
+} from "@/components/ui/card";
+import { Switch } from "@/components/ui/switch";
 
 const notifications = [
   {
@@ -37,117 +30,112 @@ const notifications = [
     // description: "2 hours ago",
   },
   {
-    title:"No Golden Rooms"
+    title: "No Golden Rooms",
   },
   {
-    title:"5 Compant listed projects Montly "
+    title: "5 Compant listed projects Montly ",
   },
   {
-    title:"Free"
-  }
-]
+    title: "Free",
+  },
+];
 
 const premium = [
-    {
-      title: "24hrs website access .",
-      // description: "1 hour ago",
-    },
-    {
-      title: "Unlimited Certifications",
-      // description: "1 hour ago",
-    },
-    {
-      title: "Unimited Project Swipes",
-      // description: "2 hours ago",
-    },
-    {
-      title:"Golden Rooms Availabes"
-    },
-    {
-      title:"20 Companny listed projects Montly "
-    },{
-        title:"Price $20"
-    }
-  ]
+  {
+    title: "24hrs website access .",
+    // description: "1 hour ago",
+  },
+  {
+    title: "Unlimited Certifications",
+    // description: "1 hour ago",
+  },
+  {
+    title: "Unimited Project Swipes",
+    // description: "2 hours ago",
+  },
+  {
+    title: "Golden Rooms Availabes",
+  },
+  {
+    title: "20 Companny listed projects Montly ",
+  },
+  {
+    title: "Price $20",
+  },
+];
 
-type CardProps = React.ComponentProps<typeof Card>
+type CardProps = React.ComponentProps<typeof Card>;
 
 export function CardDemo({ className, ...props }: CardProps) {
-
   return (
-   
-    <div className='p-40 flex'>
-    <Card className={cn("w-[380px] px-8", className)} {...props}>
-      <CardHeader>
-        <CardTitle className='text-5xl text-center'>Free</CardTitle>
-        <CardDescription className='text-center'>Free membership includes the following feautred.</CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-
-        <div>
-          {notifications.map((notifications, index) => (
-            <div
-              key={index}
-              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-            >
-              <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-              <div className="space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  {notifications.title}
-                </p>
+    <div className="p-40 flex">
+      <Card className={cn("w-[380px] px-8", className)} {...props}>
+        <CardHeader>
+          <CardTitle className="text-5xl text-center">Free</CardTitle>
+          <CardDescription className="text-center">
+            Free membership includes the following feautred.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div>
+            {notifications.map((notifications, index) => (
+              <div
+                key={index}
+                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+              >
+                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {notifications.title}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">
-          <Check className="mr-2 h-4 w-4" />
-Free plan        </Button>
-      </CardFooter>
-    </Card>
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">
+            <Check className="mr-2 h-4 w-4" />
+            Free plan{" "}
+          </Button>
+        </CardFooter>
+      </Card>
 
-
-
-    <Card className={cn("w-[380px] px-5 ml-40", className)} {...props}>
-      <CardHeader>
-        <CardTitle className='text-5xl text-center'>Premium</CardTitle>
-        <CardDescription className='text-center'>Premium membership includes the following feautred.</CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-
-        <div>
-          {premium.map((premium, index) => (
-            <div
-              key={index}
-              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-            >
-              <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-              <div className="space-y-1">
-                <p className="text-sm font-medium leading-none">
-                {premium.title}
-                </p>  
-                
+      <Card className={cn("w-[380px] px-5 ml-40", className)} {...props}>
+        <CardHeader>
+          <CardTitle className="text-5xl text-center">Premium</CardTitle>
+          <CardDescription className="text-center">
+            Premium membership includes the following feautred.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div>
+            {premium.map((premium, index) => (
+              <div
+                key={index}
+                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+              >
+                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {premium.title}
+                  </p>
+                </div>
               </div>
-             
-             
-            
-            </div>
-          ))}
-        </div>
-      </CardContent>
-      <CardFooter>
-      <Link href={`https://buy.stripe.com/test_3cs5mC3g84y8euA5kk`}>  <Button className="w-full" >
-          <Check className="mr-2 h-4 w-4" />Premium Plan
-        </Button></Link>
-      </CardFooter>
-    </Card>
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Link href={`https://buy.stripe.com/test_3cs5mC3g84y8euA5kk`}>
+            {" "}
+            <Button className="w-full">
+              <Check className="mr-2 h-4 w-4" />
+              Premium Plan
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
-
-
-
-
-    
-  )
+  );
 }
-export default CardDemo
+export default CardDemo;
--- dev-mate/src/app/projects/page.tsx
+++ dev-mate/src/app/projects/page.tsx
@@ -57,48 +57,6 @@ function ProjectCard({ project }: { project: Project }) {
   );
 }
 
-<<<<<<< HEAD
-// export default page
-// import * as React from "react"
-
-// import { Card, CardContent } from "@/components/ui/card"
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel"
-
-// export function CarouselSize() {
-//   return (
-//     <Carousel
-//       opts={{
-//         align: "start",
-//       }}
-//       className="w-full max-w-sm"
-//     >
-//       <CarouselContent>
-//         {Array.from({ length: 5 }).map((_, index) => (
-//           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-//             <div className="p-1">
-//               <Card>
-//                 <CardContent className="flex aspect-square items-center justify-center p-6">
-//                   <span className="text-3xl font-semibold">{index + 1}</span>
-//                 </CardContent>
-//               </Card>
-//             </div>
-//           </CarouselItem>
-//         ))}
-//       </CarouselContent>
-//       <CarouselPrevious />
-//       <CarouselNext />
-//     </Carousel>
-//   )
-// }
-
-export default page
-=======
 export default async function page() {
   const projects = await db.query.project.findMany();
 
@@ -116,4 +74,3 @@ export default async function page() {
     </main>
   );
 }
->>>>>>> ddc70b6827d9ee1b8baadb5ab995768df8790741
