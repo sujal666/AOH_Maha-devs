661ff8dfa0f213be0dd47ff57730c960860bf815
rahulsan007 <santrarahul7070@gmail.com>
2024-03-31 07:11:27 +0530

    comit

--- dev-mate/src/db/schema.ts
+++ dev-mate/src/db/schema.ts
@@ -122,12 +122,6 @@ export const projectWorking = pgTable("project_working", {
   projects: text("projects").array(),
   rejectedProjects: text("rejectedProjects").array(),
   previousProjects: text("previousProjects").array(),
-<<<<<<< Updated upstream
-  rejectProjects: text("rejectProjects").array(),
-
-=======
-  notWorking: text("notWorking").array(),
->>>>>>> Stashed changes
 });
 
 export type Room = typeof room.$inferSelect;
