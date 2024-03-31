cf7daa28537be1152d99315549347b179a363377
Vikas Nayak <vikasnayak96999@gmail.com>
2024-03-31 08:06:43 +0530

    sidebar change

--- dev-mate/src/components/sidebar.tsx
+++ dev-mate/src/components/sidebar.tsx
@@ -6,7 +6,7 @@ import React from "react";
 function SideBar() {
   return (
     <div>
-      <div className="w-64 h-screen bg-gray-100 dark:bg-gray-900 flex flex-col gap-5   px-14 py-4  border-r">
+      <div className="w-64 h-screen text-[20px] bg-gray-100 dark:bg-gray-900 flex flex-col gap-[60px]   px-14 py-4  border-r">
         <Link href="/dashboard"><h6 className=" flex gap-2 items-center  ">
           <LayoutDashboard />
           Dashboard
