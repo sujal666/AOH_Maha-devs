cc43e6d24a62cb69427a6ea47f5c7d1c6af28f4c
rahulsan007 <santrarahul7070@gmail.com>
2024-03-31 10:46:44 +0530

    Latest

--- dev-mate/src/app/projects-detail/[projectid]/page.tsx
+++ dev-mate/src/app/projects-detail/[projectid]/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { getProject, getRoom, isRoomUsersAllotedEmpty } from "./action";
 
 import PageComponent from "./pageComponent";
-import { getSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 
 const features = [
   { name: "Project", description: "E-Commerce." },
@@ -15,7 +15,7 @@ export default async function page(props: { params: { projectid: string } }) {
   const projectid = props.params.projectid;
   const project = await getProject(projectid);
   const roomEmpty = await isRoomUsersAllotedEmpty(projectid);
-  const session = await getSession();
+
   var rooms = await getRoom(projectid);
   if (roomEmpty) {
     console.log("room not empty");
@@ -23,11 +23,6 @@ export default async function page(props: { params: { projectid: string } }) {
   // console.log(project);
 
   return (
-    <PageComponent
-      project={project}
-      roomEmpty={roomEmpty}
-      rooms={rooms}
-      user={session?.user}
-    />
+    <PageComponent project={project} roomEmpty={roomEmpty} rooms={rooms} />
   );
 }
